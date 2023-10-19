d2c4c8978d6355bab8077c995d019b1b3d29b553
Richard Schmidt <richardnew12511@gmail.com>
2023-10-19 09:23:14 -0500

    configure harpoon

--- lua/richard/packer.lua
+++ lua/richard/packer.lua
@@ -20,5 +20,5 @@ return require('packer').startup(function(use)
     })
 
     use('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
-    use('theprimeagen/harpoon')
+    use('theprimeagen/harpoon') 
   end)
